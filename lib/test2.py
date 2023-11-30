d05c38d986f85811eaf6693cfe29aca27bd36615
Sandikha Rahardi <sandikha.rahardi@gmail.com>
2023-11-30 13:29:11 +0500

    fix number 2

--- lib/test2.py
+++ lib/test2.py
@@ -23,6 +23,7 @@ def getIdByName(db, name):
     result = db.execute(f"SELECT * FROM games WHERE name = '{name}'")
     return result.fetchone()["id"]
 
+output = []
 
 with open("assets/data/2/"+fileName+".pkl", 'rb') as f:
     allDataFile = pickle.load(f)
@@ -46,5 +47,23 @@ with open("assets/data/2/"+fileName+".pkl", 'rb') as f:
                             VALUES (?, ?, ?, ?);""",allDataDetail)
         connection.commit()
     
-    result = db.execute("SELECT games.id, games.name, city, begin, system, tours_count, min_rating, time_on_game, place, price  FROM games,detail_games WHERE games.id = detail_games.game_id")
+    result = db.execute("SELECT games.id as id, games.name as name, city, begin, system, tours_count, min_rating, time_on_game, place, price  FROM games,detail_games WHERE games.id = detail_games.game_id")
     
+    
+    for data in result.fetchall():
+        dataOutput = dict()
+        dataOutput["id"] = data["id"]
+        dataOutput["name"] = data["name"]
+        dataOutput["city"] = data["city"]
+        dataOutput["begin"] = data["begin"]
+        dataOutput["system"] = data["system"]
+        dataOutput["toursCount"] = data["tours_count"]
+        dataOutput["minRating"] = data["min_rating"]
+        dataOutput["timeOnGame"] = data["time_on_game"]
+        dataOutput["place"] = data["place"]
+        dataOutput["price"] = data["price"]
+        
+        output.append(dataOutput)
+        
+with open("assets/output/2/output.json", "w") as outfile:
+    json.dump(output, outfile, indent=4, ensure_ascii=False)
